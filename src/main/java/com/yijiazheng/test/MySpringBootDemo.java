27cca3e9bac9c9c7912ac5571e4215018adcb340
a5552157 <lk5552157@163.com>
2018-03-17 15:42:10 +0800

    first

---
+++ src/main/java/com/yijiazheng/test/MySpringBootDemo.java
@@ -0,0 +1,12 @@
+package com.yijiazheng.test;
+ org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootDemo {
+	
+	public static void main(String[] args) {
+		//项目入口
+		SpringApplication.run(MySpringBootDemo.class, args);
+	}
+}
